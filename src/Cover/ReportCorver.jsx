ecd02a7d2749576ecbe324be67581c1bcd8cb5d5
tianyang <tianyang.zhao@wiredcraft.com>
2023-01-10 17:46:10 +0800

    feat: cover

--- src/Cover/ReportCorver.jsx
+++ src/Cover/ReportCorver.jsx
@@ -1,22 +1,31 @@
 import React, { Component } from "react";
 import logo from "../wcl.png";
 
-export default class ReportCover extends Component {
-    constructor() {
-        super();
-    }
-
-    render() {
-        return (
-            <div class="p-4 h-full flex flex-col items-center">
-                <div class=" flex flex-col items-center">
-                    <img class="w-5 h-5 items-center" src={logo} alt='wcl logo' />
-                    <text class='text-black'>{"sdkName"}</text>
+function ReportCover(props) {
+    const {reportName, appName, deviceSys, fromPpace, publishDate} = props;
+    return (
+        <div class="p-4 h-screen flex flex-col items-center">
+            {/* Logo */}
+            <img class="w-20 h-20 mt-24 mb-11" src={logo} alt='wcl logo' />
+            {/* name */}
+            <text class="ml-10 mr-10 mb-11 text-center font-semibold text-3xl">{reportName}</text>
+            {/* info */}
+            <div class="flex flex-row items-center">
+                <div class="ml-6 mr-4 flex flex-col items-end text-right font-medium text-lg">
+                    <text class="mb-4">{"产品名:"}</text>
+                    <text class="mb-4">{"产品型号:"}</text>
+                    <text class="mb-4">{"报告出处:"}</text>
+                    <text class="mb-4">{"报告日期:"}</text>
                 </div>
-                <text class="grow text-center">{"title"}</text>
-                <img class="w-8 h-8" src={logo} alt='wcl logo' />
-                <text class='p-2' >{"wclName"}</text>
+                <div class="mr-6 flex flex-col items-start text-left font-medium text-lg">
+                    <text class="mb-4">{appName}</text>
+                    <text class="mb-4">{deviceSys}</text>
+                    <text class="mb-4">{fromPpace}</text>
+                    <text class="mb-4">{publishDate}</text>
+                </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+  }
+  
+  export default ReportCover;
