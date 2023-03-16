26cd64d6e06874189a219a40f92d4a30c9f8fe63
tianyang <tianyang.zhao@wiredcraft.com>
2023-03-16 10:07:15 +0800

    feat: power usage

--- src/components/PowerUsageChart/index.jsx
+++ src/components/PowerUsageChart/index.jsx
@@ -57,8 +57,14 @@ const PowerUsageChart = () => {
     const { locationData } = performanceData;
     const sortNetworkFlowData = networkFlowData.sort((a, b) => (a.time - b.time));
     const sortLocationData = locationData.sort((a, b) => (a.time - b.time));
+    const networkTotalTime = sortNetworkFlowData.reduce(function (sum, item) {
+        return sum + item.duration;
+    }, 0);
+    const locationTotalTime = sortLocationData.reduce(function (sum, item) {
+        return sum + item.duration;
+    }, 0);
 
-    var categories = [{ name: "Network", color: "#72b362", data: sortNetworkFlowData }, { name: "Gps", color: "#dc77dc", data: sortLocationData }];
+    var categories = [{ name: "Network", color: "#72b362", data: sortNetworkFlowData }, { name: "GPS", color: "#dc77dc", data: sortLocationData }];
 
     const startTime = getStartTime(sortNetworkFlowData.map(e => e.time), sortLocationData.map(e => e.time))
     const endTime = getEndTime(sortNetworkFlowData.map(e => e.time), sortLocationData.map(e => e.time))
@@ -133,7 +139,7 @@ const PowerUsageChart = () => {
                 formatter: function (val) {
                     if (Math.abs((val - startTime)) > 1000 && Math.abs((endTime - val)) > 1000) return "";
                     return dayjs(val).format('HH:mm');
-                  }
+                }
             },
         },
         yAxis: {
@@ -179,6 +185,90 @@ const PowerUsageChart = () => {
         ]
     };
     const chartImage = getChartsBlobImage(option);
+    const impactOfPowerUsage = `
+    1, power consumption will affect the user experience: apps with high power consumption will consume more power, which will lead to rapid power consumption of the phone and affect the user experience.
+
+    2, power consumption will reduce the performance of the phone: if an app's power consumption is too large, it will take up system resources, thus reducing the performance of the phone.
+
+    3. High power consumption may cause damage to the phone: If an app consumes too much power, it may cause the phone to heat up too much, damage internal components and other problems.
+    `
+    const beginDate = dayjs(startTime).format('YYYY MM/DD HH:mm');
+    const endDate = dayjs(endTime).format('YYYY MM/DD HH:mm');
+    const dataSourceDes = `The data for the above chart comes from app usage between ${beginDate} and ${endDate}`
+
+    var countPieDatas = [
+        { name: 'Network', value: sortNetworkFlowData.length },
+        { name: 'GPS', value: sortLocationData.length },
+    ];
+    const countPieChartOption = {
+        series: [
+            {
+                type: 'pie',
+                radius: [20, 60],
+                itemStyle: {
+                    borderColor: '#fff',
+                    borderWidth: 1
+                },
+                label: {
+                    alignTo: 'edge',
+                    formatter: '{name|{b}}\n{time|{c} times}',
+                    minMargin: 5,
+                    edgeDistance: 10,
+                    lineHeight: 15,
+                    rich: {
+                        time: {
+                            fontSize: 10,
+                            color: '#999'
+                        }
+                    }
+                },
+                labelLine: {
+                    length: 15,
+                    length2: 0,
+                    maxSurfaceAngle: 80
+                },
+                data: countPieDatas
+            }
+        ]
+    };
+    const countPieImage = getChartsBlobImage(countPieChartOption);
+
+    var timePieDatas = [
+        { name: 'Network', value: Math.round(networkTotalTime) },
+        { name: 'GPS', value: Math.round(locationTotalTime) },
+    ];
+    const timePieChartOption = {
+        series: [
+            {
+                type: 'pie',
+                radius: [20, 60],
+                itemStyle: {
+                    borderColor: '#fff',
+                    borderWidth: 1
+                },
+                label: {
+                    alignTo: 'edge',
+                    formatter: '{name|{b}}\n{time|{c} ms}',
+                    minMargin: 5,
+                    edgeDistance: 10,
+                    lineHeight: 15,
+                    rich: {
+                        time: {
+                            fontSize: 10,
+                            color: '#999'
+                        }
+                    }
+                },
+                labelLine: {
+                    length: 15,
+                    length2: 0,
+                    maxSurfaceAngle: 80
+                },
+                data: timePieDatas
+            }
+        ]
+    };
+    const timePieImage = getChartsBlobImage(timePieChartOption);
 
     return (
         <View bookmark={{ title: "Chapter 2: Power Usage", fit: true }} break>
@@ -186,8 +276,13 @@ const PowerUsageChart = () => {
                 <Text style={styles.sectionsChapter}>Chapter 2</Text>
                 <Text style={styles.sectionsTitle} id='link_power'>Power Usage</Text>
                 <Text style={styles.text}>Network requests and GPS location both affect the app's power consumption. The more network requests, the more data requested and the more time spent on requests, the more power is consumed, and the more positioning is used and the longer it is used, the more power is consumed.</Text>
-                <Text style={styles.hint}>The x-axis represents time and the y-axis represents the hardware used, with denser lines on the way indicating more frequent use and wider lines indicating longer use</Text>
+                <Text style={styles.text}>What will be the impact of the app's power consumption?</Text>
+                <Text style={styles.text}>{impactOfPowerUsage}</Text>
                 <View style={styles.chartContainer}><Image src={chartImage} /></View>
+                <Text style={styles.hint}>The x-axis represents time and the y-axis represents the hardware used, with denser lines on the way indicating more frequent use and wider lines indicating longer use</Text>
+                <View style={styles.chartContainer}><Image src={countPieImage} /></View>
+                <View style={styles.chartContainer}><Image src={timePieImage} /></View>
+                <Text style={styles.text}>{dataSourceDes}</Text>
             </View>
         </View>
     )
