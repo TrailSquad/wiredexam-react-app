05d2b6dc11a17bea7a18ac163a0054ce577ed3fd
shawn <zhang.shawn@externals.adidas.com>
2023-03-16 12:03:45 +0800

    feat: add dotted line in contents page

--- src/components/Contents/index.js
+++ src/components/Contents/index.js
@@ -13,16 +13,16 @@ const Contents = () => {
     <View style={styles.pageContainer} bookmark={{ title: "Contents", fit: true }}>
       <Text style={styles.contentsTitle}>Contents</Text>
       <View style={{ width: "80%", flexDirection: "row" }}>
-        <View style={{ width: "80%" }}>
-          <Link style={styles.contentsItem} src='#link_overview'>Overview</Link>
-          <Link style={styles.contentsItem} src='#link_fps'>1. FPS</Link>
-          <Link style={styles.contentsItem} src='#link_power'>2. Power Usage</Link>
-          <Link style={styles.contentsSubItem} src='#link_network'>2.1. Network Monitoring</Link>
-          <Link style={styles.contentsSubItem} src='#link_gps'>2.2. GPS Positioning</Link>
-          <Link style={styles.contentsItem} src='#link_launch'>3. Launch Time</Link>
-          <Link style={styles.contentsItem} src='#link_memory'>4. Memory Leak</Link>
+        <View style={{ width: "90%" }}>
+          <Link style={styles.contentsItem} src='#link_overview'>Overview   … … … … … … … … … … … … … … … … … … … …</Link>
+          <Link style={styles.contentsItem} src='#link_fps'>1. FPS  … … … … … … … … … … … … … … … … … … … … … … … …</Link>
+          <Link style={styles.contentsItem} src='#link_power'>2. Power Usage  … … … … … … … … … … … … … … … … … … …</Link>
+          <Link style={styles.contentsSubItem} src='#link_network'>2.1. Network Monitoring  … … … … … … … … … … … …</Link>
+          <Link style={styles.contentsSubItem} src='#link_gps'>2.2. GPS Positioning  … … … … … … … … … … … … … … … …</Link>
+          <Link style={styles.contentsItem} src='#link_launch'>3. Launch Time  … … … … … … … … … … … … … … … … … … …</Link>
+          <Link style={styles.contentsItem} src='#link_memory'>4. Memory Leak  … … … … … … … … … … … … … … … … … … …</Link>
         </View>
-        <View style={{ width: "20%" }}>
+        <View style={{ width: "10%" }}>
           <Text style={styles.contentsPage}>3</Text>
           <Text style={styles.contentsPage}>4</Text>
           <Text style={styles.contentsPage}>7</Text>
