d55bf9d1f687af5bd2d51111f0ee8f3d1dfe98f4
XERO <xerolry@gmail.com>
2023-04-27 13:57:37 +0800

    refactor: data structure

--- src/components/PdfDocument/index.js
+++ src/components/PdfDocument/index.js
@@ -18,7 +18,8 @@ import styles from "src/pdfStyles";
 import Conclusion from "../Conclusion";
 import BackCover from "../BackCover";
 import { memo, useState, useCallback } from "react";
-import { DIRECTORIES } from "src/constants/directories";
+import Constants from "src/constants";
+
 // 默认只支持拉丁英文，中文字体一定要注入
 // 方正黑体相对来说非常小（3M），而且是可免费商用无书面授权的字体，因此采用
 Font.register({
@@ -77,63 +78,61 @@ const PDFDocument = ({ performanceData }) => {
   const [memoryIndex, setMemoryIndex] = useState(0);
   const [endIndex, setEndIndex] = useState(0);
 
-
-
   const CONTENT_ITEMS = [
     {
       index: null,
       title: "Overview   … … … … … … … … … … … … … … … … … … … …",
       source: "#link_overview",
       pageIndex: 3,
-      key: "overView",
+      key: Constants.directories.DIRECTORIES_KEY_OVER_VIEW
     },
     {
       index: null,
       title: "ANR  … … … … … … … … … … … … … … … … … … … … … … … …",
       source: "#link_ANR",
       pageIndex: 5,
-      key: "anr",
+      key: Constants.directories.DIRECTORIES_KEY_ANR
     },
     {
       index: null,
       title: "Power Usage  … … … … … … … … … … … … … … … … … … …",
       source: "#link_power",
       pageIndex: powerIndex,
-      key: "powerUsage",
+      key: Constants.directories.DIRECTORIES_KEY_POWER_USAGE
     },
     {
       index: null,
       title: "Launch Time  … … … … … … … … … … … … … … … … … … …",
       source: "#link_launch",
       pageIndex: launchTimeIndex,
-      key: "launchTime",
+      key: Constants.directories.DIRECTORIES_KEY_LAUNCH_TIME
     },
     {
       index: null,
       title: "Page Load Time  … … … … … … … … … … … … … … … …",
       source: "#link_page_load",
       pageIndex: pageLoadTimeIndex,
-      key: "pageLoadTime",
+      key: Constants.directories.DIRECTORIES_KEY_PAGE_LOAD_TIME
     },
     {
       index: null,
       title: "Memory Leak  … … … … … … … … … … … … … … … … … … …",
       source: "#link_memory",
       pageIndex: memoryIndex,
-      key: "memoryLeak",
+      key: Constants.directories.DIRECTORIES_KEY_MEMORY_LEAK
     },
     {
       index: null,
       title: "About  … … … … … … … … … … … … … … … … … … …",
       source: "#link_back_cover",
       pageIndex: endIndex,
-      key: "about",
+      key: Constants.directories.DIRECTORIES_KEY_ABOUT
     },
   ];
 
   const displayContents = [];
 
-  DIRECTORIES[performanceData.platform].forEach((i, idx) => {
+  Constants.directories.DIRECTORIES_PLATFORMS[performanceData.platform].forEach((i, idx) => {
     CONTENT_ITEMS.forEach((j) => {
       if (j.key === i) {
         displayContents.push({
--- src/constants/directories.js
+++ src/constants/directories.js
@@ -1,22 +1,24 @@
   // refer to the key of CONTENT_ITEMS
-  export const DIRECTORIES = {
-    ios: [
-      "overView",
-      "anr",
-      "powerUsage",
-      "launchTime",
-      "pageLoadTime",
-      "memoryLeak",
-      "about",
-    ],
-    android: [
-      "overView",
-      "anr",
-      "powerUsage",
-      "launchTime",
-      "pageLoadTime",
-      "memoryLeak",
-      "about",
-    ],
-    flutter: [],
+  export const DIRECTORIES_KEY_OVER_VIEW = "overView"
+  export const DIRECTORIES_KEY_ANR = "anr"
+  export const DIRECTORIES_KEY_POWER_USAGE = "powerUsage"
+  export const DIRECTORIES_KEY_LAUNCH_TIME = "launchTime"
+  export const DIRECTORIES_KEY_PAGE_LOAD_TIME = "pageLoadTime"
+  export const DIRECTORIES_KEY_MEMORY_LEAK = "memoryLeak"
+  export const DIRECTORIES_KEY_ABOUT = "about"
+
+  export const directories = [
+    DIRECTORIES_KEY_OVER_VIEW,
+    DIRECTORIES_KEY_ANR,
+    DIRECTORIES_KEY_POWER_USAGE,
+    DIRECTORIES_KEY_LAUNCH_TIME,
+    DIRECTORIES_KEY_PAGE_LOAD_TIME,
+    DIRECTORIES_KEY_MEMORY_LEAK,
+    DIRECTORIES_KEY_ABOUT
+  ];
+  
+  export const DIRECTORIES_PLATFORMS = {
+    ios: [...directories],
+    android: [...directories],
+    flutter: []
   };
